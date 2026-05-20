0975607ad38aa88f58be634c55b00aba7bd3aab0
Chernousova Angelina <chernousova.a.v@edu.mirea.ru>
2026-05-20 16:32:14 +0300

    исправление некоторых комментариев

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
   const [user, setUser] = useState(() => {
     const saved = localStorage.getItem('gallery_user');
